f6964582fc3cb059dfe16e3454d288a7b27d53e3
Mukhtar <steklofan@gmail.com>
2018-04-09 23:16:58 +0200

    Added comment Athorization

--- routes/comments.js
+++ routes/comments.js
@@ -45,7 +45,7 @@ router.post("/", isLoggedIn, function (req, res) {
   
 })
 // NESTED COMMENT EDIT ROUTES
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, foundComment) {
     if (err) {
       res.redirect("back");
@@ -55,7 +55,7 @@ router.get("/:comment_id/edit", function(req, res) {
   })
 })
 // Comment UPDATE
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
     if (err) {
       res.redirect('back');
@@ -66,7 +66,7 @@ router.put("/:comment_id", function(req, res) {
 })
 
 // COMMENTS DESTROY ROUTE
-router.delete('/:comment_id', function(req, res) {
+router.delete('/:comment_id', checkCommentOwnership, function(req, res) {
   Comment.findByIdAndRemove(req.params.comment_id, function(err) {
     if (err) {
       res.redirect('back')
@@ -83,6 +83,27 @@ function isLoggedIn(req, res, next) {
     return next();
   } 
   res.redirect('/login');
+}
+
+function checkCommentOwnership(req, res, next) {
+  // is user logged in?
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if (err) {
+        res.redirect("back");
+      } else {
+        // does user own this comment //equals() is a mongoose method
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect('back')
+        }
+      }
+    });
+  } else {    
+  // if not, redirect
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
